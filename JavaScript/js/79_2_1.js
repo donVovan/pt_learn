172d9966f2d47bf9676321f4c4e74bfe5e0aea99
donVovan <vladimir.a.blinov@gmail.com>
2022-04-09 22:02:20 +0300

    js 79 - 80

---
+++ JavaScript/js/79_2_1.js
@@ -0,0 +1,8 @@
+'use strict';
+let result = '';
+for (let i = 1; i <= 30; i++) {
+    result += i;
+}
+
+document.write(result);
+console.log(result)
--- JavaScript/js/80_1_1.js
+++ JavaScript/js/80_1_1.js
@@ -1,8 +1,6 @@
 'use strict';
-for (let i = 10; i <= 33; i = 10 + i) {
-    
+for (let i = 1; i <= 9; i++) {
     for (let j = 1; j <= 3; j++) {
-        
         document.write(i);
     }
 }
